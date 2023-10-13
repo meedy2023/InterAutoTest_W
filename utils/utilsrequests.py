8152dd0ec374667a4c34825f7fed82908991a9d6
DESKTOP-P4IIBGL\zhaowei <zhaowei1@playdayy.com>
2023-10-13 14:04:29 +0800

    第1次提交

---
+++ utils/utilsrequests.py
@@ -0,0 +1,34 @@
+# 作者:meedy
+# 编写时间：2023/10/13  13:28
+# 代码内容：requests方法封装
+#1第一步：创建封装request.get()方法
+import requests
+
+def reqests_get(url,headers=None,json=None):
+    r = requests.get(url,headers=headers)
+    code = r.status_code
+    try:
+
+        body = r.json()
+    except Exception as e:
+        body = r.text
+    res = dict()
+    res["code"] = code
+    res["body"] = body
+    return  res
+
+def reqests_post(url,json=None,headers=None):
+    r = requests.post(url,json=json,headers=headers)
+    code = r.status_code
+    try:
+
+        body = r.json()
+    except Exception as e:
+        body = r.text
+    res = dict()
+    res["code"] = code
+    res["body"] = body
+    return  res
+
+
+
