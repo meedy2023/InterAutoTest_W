d7177f4744a43ba88fcc6a932f96cd4530987e83
DESKTOP-P4IIBGL\zhaowei <zhaowei1@playdayy.com>
2023-10-13 12:10:07 +0800

    第1次提交

---
+++ test_case/test_login.py
@@ -0,0 +1,21 @@
+# 作者:meedy
+# 编写时间：2023/10/13  11:09
+# 代码内容：登录购物网站
+import requests
+import urllib
+def login():
+
+    url = 'http://admin.5istudy.online/login/'
+    data =  {
+        "username": '13811200000',
+        "password": '123456'
+    }
+
+    r = requests.post(url,json=data)
+    print(r.json())
+    token = r.json()["token"]
+    print(r.json()["token"])
+
+
+if __name__ == '__main__':
+    login()
---
+++ test_case/test_userinfo.py
@@ -0,0 +1,26 @@
+# 作者:meedy
+# 编写时间：2023/10/13  11:09
+# 代码内容：登录购物网站
+import requests
+import urllib
+def user_info():
+#第一步登录，拿到token
+    url1 = 'http://admin.5istudy.online/login/'
+    data =  {
+        "username": '13811200000',
+        "password": '123456'
+    }
+
+    r = requests.post(url1,json=data)
+    #print(r.json())
+    print(r.json()["token"])
+    token = r.json()["token"]
+#第二部获取用户基本信息
+    url2 = 'http://admin.5istudy.online/users/1/'
+
+    headers ={'Authorization':'JWT '+ token }
+    s = requests.get(url2,headers=headers)
+    print(s.json())
+if __name__ == '__main__':
+
+    user_info()
